8245863328150dc367df4f2fd9ce61818d6a62d4
GestPro <holikou08@gmail.com>
2026-04-13 19:06:56 +0000

    Nettoyage fichiers inutiles

--- frontend/pred.py
+++
@@ -1,3 +0,0 @@
-lines.append('import React, { useEffect, useState } from react')
-lines.append('import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend } from recharts')
-lines.append('import axios from axios')
